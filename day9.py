12c6154524066f2814b455d68b678622d21ccf85
Robert Fennis <rfennis92@gmail.com>
2022-12-09 08:07:47 +0100

    day9

---
+++ day9.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+import aoc as aoc
+
+lines = aoc.read_datafile("day9.txt")
+
+N = 10
+
+rope = [np.array([0, 0]) for _ in range(N)]
+
+
+dirs = {
+    "R": np.array([1, 0]),
+    "L": np.array([-1, 0]),
+    "U": np.array([0, 1]),
+    "D": np.array([0, -1]),
+}
+points = set()
+for line in lines:
+    dr, vl = line.split(" ")
+    ds = dirs[dr]
+    for i in range(int(vl)):
+        rope[0] = rope[0] + ds
+        for k, T in enumerate(rope[1:]):
+            dt = rope[k] - T
+            DT = np.sign(dt)
+            if np.max(np.abs(dt)) > 1:
+                rope[k + 1] = rope[k + 1] + DT
+        points.add(tuple([int(x) for x in rope[-1]]))
+print(len(points))
